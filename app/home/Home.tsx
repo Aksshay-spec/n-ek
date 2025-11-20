6c329228e8bac04a15c5d1279e6cdacfe94afeb0
Aksshay-spec <umidphp.akshay@.com>
2025-11-20 16:00:10 +0530

    created home

---
+++ app/home/Home.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+
+import HomeSlider from "../../components/home/HomeSlider";
+import TrendingProducts from "../../components/home/TrendingProducts";
+import SkuSection from "../../components/home/SkuSection";
+import AboutSection from "../../components/home/AboutSection";
+import AboutListings from "../../components/home/AboutListings";
+import FmcgSection from "../../components/home/FmcgSection";
+import SeventhSection from "../../components/home/SeventhSection";
+import OurJourney from "../../components/home/OurJourney";
+import PartnerWithUsSection from "../../components/home/PartnerWithUsSection";
+import DistributorBenefitSection from "../../components/home/DistributorBenefitSection";
+import Testimonial from "../../components/home/Testimonial";
+import OurNews from "../../components/home/OurNews";
+import Policies from "../../components/home/Policies";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <HomeSlider />
+      <TrendingProducts />
+      <SkuSection />
+      <AboutSection />
+      <AboutListings />
+      <FmcgSection />
+      <SeventhSection />
+      <OurJourney />
+      <PartnerWithUsSection />
+      <DistributorBenefitSection />
+      <Testimonial />
+      <OurNews />
+      <Policies />
+    </>
+  );
+};
+
+export default Home;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Home from "@/components/home/Home";
+import Home from "@/app/home/Home";
 
 export default function Page() {
   return <Home />;
