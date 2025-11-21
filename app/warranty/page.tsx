567fbe33f751c8b03ea3caf14ed63c4455c214a5
Aksshay-spec <umidphp.akshay@.com>
2025-11-21 10:09:29 +0530

    sections updated

--- app/warranty/page.tsx
+++ app/warranty/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import Navigation from "@/components/navigator/MobileHeader";
-import FooterNavigation from "@/components/navigator/MobileFooter";
+
 import HeroSection from "@/app/warranty/HeroSection";
 
 export default function Warranty() {
